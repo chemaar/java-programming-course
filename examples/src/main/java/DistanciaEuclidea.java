2126041690a8a2c596d60caac358c55054f58bf2
chemaar <chema.ar@gmail.com>
2015-10-08 08:44:00 +0200

    Adding examples

---
+++ examples/src/main/java/DistanciaEuclidea.java
@@ -0,0 +1,19 @@
+/**
+ * Escribir un programa que calcule la distancia entre dos puntos.
+ * 
+ * @author chema
+ *
+ */
+public class DistanciaEuclidea {
+
+	public static void main(String []args){
+		double x1 = 2.0;
+		double y1 = 3.0;
+		double x2 = 4.0;
+		double y2 = 5.0;
+
+		double distancia   = Math.sqrt(Math.pow(x1-x2,2) + Math.pow(y1-y2,2));
+		
+		System.out.println("Distancia = " + distancia);  
+	}
+}
